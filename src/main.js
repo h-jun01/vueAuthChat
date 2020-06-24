a643bf6884ca248e9a9d4e9f33c91d44a49cf75b
橋本准 <hashimotojun@jun.local>
2020-06-25 00:30:10 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import firebase from "firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+var Config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+// Initialize Firebase
+firebase.initializeApp(Config);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
